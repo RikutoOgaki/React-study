a8dcb9133c6028a9b0edc2369fead305c34f1bb7
Rikuto <orikutobasuke@gmail.com>
2023-02-26 21:42:03 +0900

    sample 追加

--- study/src/main.tsx
+++ study/src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Sample01 from './sample/01/sample01'
+import Sample02 from './sample/02/sample02'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Sample01 />
+    {/* <Sample01 /> */}
+    <Sample02/>
   </React.StrictMode>,
 )
---
+++ study/src/sample/02/sample02.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+
+function rollDice() {
+    return Math.floor(Math.random()*6 + 1)
+}
+
+function sample02() {
+
+    const [state,setState] = useState(0)
+
+    useEffect(()=>{
+        setState(rollDice())
+    },[])
+
+  return (
+    <>
+        <h1>Sample02</h1>
+
+        <p>サイコロの出目:{state}</p>
+        
+        <input type="button" value="Diceroll" 
+            onClick={()=> setState(rollDice())}
+        />
+    </>
+  )
+}
+
+export default sample02
