32cdddd21ef7402db20fc97ba5269f76ea79558d
Rikuto <orikutobasuke@gmail.com>
2023-03-07 16:13:05 +0900

    React study

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Sample01 from './sample/01/sample01'
 import Sample02 from './sample/02/sample02'
+import Sample03 from './sample/03/sample03'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     {/* <Sample01 /> */}
-    <Sample02/>
+    {/* <Sample02/> */}
+    <Sample03/>
   </React.StrictMode>,
 )
