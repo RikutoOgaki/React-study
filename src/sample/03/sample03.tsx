47b70739543152098ec400cbea23b61505c1d7d8
Rikuto <orikutobasuke@gmail.com>
2023-03-14 12:16:23 +0900

    React study

--- src/sample/03/sample03.tsx
+++ src/sample/03/sample03.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+type State = {
+  a: string,
+  b: string,
+  result: number
+}
 
 function sample03() {
+
+  const [state, setState] = useState({
+    a: '',
+    b: '',
+    result: 0
+  })
+
   return (
     <div>
-        <h1>hello world</h1>
+      <h1>inputタグを使った計算</h1>
+      <h2>{state.result}</h2>
+      <input type="text" value={state.a}
+        onChange={(e) => setState({
+          ...state,
+          a: e.target.value
+        })}
+      />
+
+      <input type="text" value={state.b}
+        onChange={(e) => setState({
+          ...state,
+          b: e.target.value
+        })}
+      />
+
+      <input type="button" value={'計算'}
+        onClick={() => {
+          let numA = parseInt(state.a)
+          let numB = parseInt(state.b)
+
+          setState({
+            ...state,
+            result: numA + numB
+          })
+        }}
+      />
     </div>
   )
 }
