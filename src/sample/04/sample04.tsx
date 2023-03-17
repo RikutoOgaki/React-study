7c266364115afff5622d116366bd841aca158a0b
Rikuto <orikutobasuke@gmail.com>
2023-03-17 14:43:35 +0900

    React study

---
+++ src/sample/04/sample04.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+function sample04() {
+
+    const [state, setState] = useState()
+
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
+
+export default sample04
